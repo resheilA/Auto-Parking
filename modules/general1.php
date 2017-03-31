310526f337126fe532d254cdad7dc12f87bc82b1
Resheil Agarwal <resheil@resheil.com>
2017-04-01 04:42:06 +0530

    Update Modules
    Blocking domain name, renew domain names , changing password, blocking
    user added to the list.

--- modules/general1.php
+++ modules/general1.php
@@ -6,7 +6,7 @@ function update($tablename, $tablerow, $value, $condition){
 //include("database/connectdb.php"); 
 connectdb();
  
-echo $query = "UPDATE $tablename SET $tablerow = $value WHERE $condition";	
+echo $query = "UPDATE $tablename SET $tablerow = '$value' WHERE $condition";	
 
 $result=mysql_query($query);
 	if(! $result)
@@ -15,5 +15,52 @@ $result=mysql_query($query);
 	}	
 }
 
-update("domain_added", "Blocked", "0", "domain_name = 'webportal.in'");
+function blockdomain($domainame){
+update("domain_added", "Blocked", "1", "domain_name = '$domainame'");
+}
+
+
+function unblockdomain($domainame){
+update("domain_added", "Blocked", "0", "domain_name = '$domainame'");
+}
+
+function renewdomain($domainname, $year){
+connectdb();
+
+// This part has to be later modified with select queries. Function retrieves deleted date. 
+ $sql = "SELECT Time_deleted FROM domain_added where domain_name = '$domainname';"
+											 or die("Couldnt find the table");
+											 
+											 $sql_result = mysql_query($sql) or die ( mysql_error());
+											 										
+											 while($row = mysql_fetch_array($sql_result))
+											 {
+											$deleteDate=$row["Time_deleted"];										
+											}
+								
+								
+date_default_timezone_set("UTC");
+$time = date('Y-m-d H:i:s', strtotime('+'.$year.' years', strtotime($deleteDate)));
+update("domain_added","Time_deleted", "$time", "domain_name = '$domainname'");
+
+}
+
+
+
+function changepassword($username, $password){
+connectdb();
+update("user", "Password", md5($password), "Username = '$username'");
+}
+
+function blockuser($username){
+update("user", "Blocked", "1", "Username = '$username'");
+}
+
+
+function unblockuser($username){
+update("user", "Blocked", "0", "Username = '$username'");
+}
+
+unblockuser("Resheil");
+//renewdomain("hacker.in","1");   ---- Check renew domains
 ?>
